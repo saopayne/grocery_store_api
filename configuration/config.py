5d02a3f19fa8ff32d039183961dcaaebfe4d11d9
Ademola Oyewale <saopayne@gmail.com>
2018-11-12 18:19:31 +0100

    Add heroku DB url

--- configuration/config.py
+++ configuration/config.py
@@ -24,7 +24,8 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SECRET = "development secret"
     SQLALCHEMY_DATABASE_URI = os.getenv('RIDECO_DATABASE_URL') or \
-                              "mysql://root:saopayne@localhost:3306/rideco_grocery_db"
+                              "mysql://b1587976a48c40:11f54bd2@us-cdbr-iron-east-01.cleardb.net/heroku_93575e569e5df3b?reconnect=true"
+                              # "mysql://root:saopayne@localhost:3306/rideco_grocery_db"
 
 
 class TestingConfig(Config):
@@ -32,7 +33,8 @@ class TestingConfig(Config):
     SECRET = "development secret"
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.getenv('RIDECO_TEST_DATABASE_URL') or \
-                              "mysql://root:saopayne@localhost:3306/rideco_grocery_test_db"
+                              "mysql://b1587976a48c40:11f54bd2@us-cdbr-iron-east-01.cleardb.net/heroku_93575e569e5df3b?reconnect=true"
+                              # "mysql://root:saopayne@localhost:3306/rideco_grocery_test_db"
 
 
 class StagingConfig(Config):
